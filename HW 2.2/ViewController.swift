f7d66df34804814816a30b25d38999a13009f2ac
Konstantin Ermolenko <ermolenko.konst@yandex.ru>
2020-02-02 23:01:26 +0300

    Настроил элементы установки всех трёх компонент цвета. Отображение значения слайдера в поле и лейбле. Изменение цвета  View

--- HW 2.2/ViewController.swift
+++ HW 2.2/ViewController.swift
@@ -11,11 +11,39 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var colorView: UIView!
+    
     @IBOutlet var redValueLabel: UILabel!
-    @IBOutlet var redValue: UITextField!
     @IBOutlet var redSlider: UISlider!
+    @IBOutlet var redValueField: UITextField!
+    
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var greenValueField: UITextField!
+    
+    @IBOutlet var blueValueLabel: UILabel!
+    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet var blueValueField: UITextField!
+        
+    private var redComponent: CGFloat = CGFloat(1.0)
+    private var greenComponent: CGFloat = CGFloat(1.0)
+    private var blueComponent: CGFloat = CGFloat(1.0)
+    private var alphaValue: CGFloat = CGFloat(1.0)
     
     let numberFormatter = NumberFormatter()
+    
+    fileprivate func setInitialComponentValues() {
+        var textValue = numberFormatter.string(from: NSNumber(value: redSlider.value))
+        redValueLabel.text = textValue
+        redValueField.text = textValue
+        
+        textValue = numberFormatter.string(from: NSNumber(value: greenSlider.value))
+        greenValueLabel.text = textValue
+        greenValueField.text = textValue
+        
+        textValue = numberFormatter.string(from: NSNumber(value: blueSlider.value))
+        blueValueLabel.text = textValue
+        blueValueField.text = textValue
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,20 +51,44 @@ class ViewController: UIViewController {
         numberFormatter.maximumFractionDigits = 2
         numberFormatter.minimumFractionDigits = 2
         
-        redValueLabel.text = numberFormatter.string(from: NSNumber(value: redSlider.value))
-        
+        setInitialComponentValues()
     }
 
     override func viewWillLayoutSubviews() {
-        colorView.layer.cornerRadius = 10
+        colorView.layer.cornerRadius = 15
+        setViewColor()
     }
     
-    @IBAction func redSliderAction() {
+    func setViewColor() {
+        colorView.backgroundColor = UIColor(red: redComponent, green: greenComponent, blue: blueComponent, alpha: alphaValue)
+    }
+    
+    @IBAction func redSliderChanged() {
         redSlider.value = (redSlider.value * 100).rounded() / 100
-        redValueLabel.text = numberFormatter.string(from: NSNumber(value: redSlider.value))
+        redComponent = CGFloat(redSlider.value)
+        
+        let textValue = numberFormatter.string(from: NSNumber(value: redSlider.value))
+        redValueLabel.text = textValue
+        redValueField.text = textValue
+        
+        setViewColor()
     }
     
+    @IBAction func greenSliderChanged() {
+        greenSlider.value = (greenSlider.value * 100).rounded() / 100
+        greenValueLabel.text = numberFormatter.string(from: NSNumber(value: greenSlider.value))
+        
+        greenComponent = CGFloat(greenSlider.value)
+        setViewColor()
+    }
     
+    @IBAction func blueSliderChanged() {
+        blueSlider.value = (blueSlider.value * 100).rounded() / 100
+        blueValueLabel.text = numberFormatter.string(from: NSNumber(value: blueSlider.value))
+        
+        blueComponent = CGFloat(blueSlider.value)
+        setViewColor()
+    }
     
 }
 
