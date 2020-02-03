c17602731711cbeda2eae46518a2e8b55c699c19
Konstantin Ermolenko <ermolenko.konst@yandex.ru>
2020-02-04 00:43:22 +0300

    Ввод значений с клавиатуры готов

--- HW 2.2/ViewController.swift
+++ HW 2.2/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // MARK: - IB
     @IBOutlet var colorView: UIView!
     
     @IBOutlet var redValueLabel: UILabel!
@@ -27,7 +28,6 @@ class ViewController: UIViewController {
     private var redComponent: CGFloat = CGFloat(1.0)
     private var greenComponent: CGFloat = CGFloat(1.0)
     private var blueComponent: CGFloat = CGFloat(1.0)
-    private var alphaValue: CGFloat = CGFloat(1.0)
     
     let numberFormatter = NumberFormatter()
     
@@ -52,15 +52,37 @@ class ViewController: UIViewController {
         numberFormatter.minimumFractionDigits = 2
         
         setInitialComponentValues()
+        
+        redValueField.delegate = self
+        greenValueField.delegate = self
+        blueValueField.delegate = self
+    }
+    
+    func setupTextFields() {
+        let toolbar = UIToolbar(frame: CGRect(origin: .zero, size: .init(width: view.frame.size.width, height: 30)))
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneBtn = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(doneButtonAction))
+        
+        toolbar.setItems([flexSpace, doneBtn], animated: false)
+        toolbar.sizeToFit()
+        
+        redValueField.inputAccessoryView = toolbar
+        greenValueField.inputAccessoryView = toolbar
+        blueValueField.inputAccessoryView = toolbar
     }
 
+    @objc func doneButtonAction() {
+        self.view.endEditing(true)
+    }
+    
     override func viewWillLayoutSubviews() {
         colorView.layer.cornerRadius = 15
         setViewColor()
+        setupTextFields()
     }
     
     func setViewColor() {
-        colorView.backgroundColor = UIColor(red: redComponent, green: greenComponent, blue: blueComponent, alpha: alphaValue)
+        colorView.backgroundColor = UIColor(red: redComponent, green: greenComponent, blue: blueComponent, alpha: CGFloat(1.0))
     }
     
     @IBAction func redSliderChanged() {
@@ -76,22 +98,56 @@ class ViewController: UIViewController {
     
     @IBAction func greenSliderChanged() {
         greenSlider.value = getRoundedValue(greenSlider.value)
-        greenValueLabel.text = numberFormatter.string(from: NSNumber(value: greenSlider.value))
-        
         greenComponent = CGFloat(greenSlider.value)
+        
+        let textValue = numberFormatter.string(from: NSNumber(value: greenSlider.value))
+        greenValueLabel.text = textValue
+        greenValueField.text = textValue
+        
         setViewColor()
     }
     
     @IBAction func blueSliderChanged() {
         blueSlider.value = getRoundedValue(blueSlider.value)
-        blueValueLabel.text = numberFormatter.string(from: NSNumber(value: blueSlider.value))
-        
         blueComponent = CGFloat(blueSlider.value)
+        
+        let textValue = numberFormatter.string(from: NSNumber(value: blueSlider.value))
+        blueValueLabel.text = textValue
+        blueValueField.text = textValue
+        
         setViewColor()
     }
     
     func getRoundedValue(_ value: Float) -> Float {
         return (value * 100).rounded() / 100
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        guard let textValue = textField.text else { return }
+        
+        var decimalValue = (textValue as NSString).floatValue
+        decimalValue = decimalValue <= 1.0 ? getRoundedValue(decimalValue) : 1.0
+        
+        let labelText = numberFormatter.string(from: NSNumber(value: decimalValue))
+        
+        if textField == redValueField {
+            redComponent = CGFloat(decimalValue)
+            redSlider.value = decimalValue
+            redValueLabel.text = labelText
+        } else if textField == greenValueField {
+            greenComponent = CGFloat(decimalValue)
+            greenSlider.value = decimalValue
+            greenValueLabel.text = labelText
+        } else if textField == blueValueField {
+            greenComponent = CGFloat(decimalValue)
+            greenSlider.value = decimalValue
+            greenValueLabel.text = labelText
+        }
+        setViewColor()
     }
 }
 
